8b83d59e13c0f3babd9a537009db5602cd9c4172
dic <9glekr@gmail.com>
2021-10-09 12:36:47 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client';
-import TextField from '@material-ui/core/TextField';
 import './App.css';
 import './style.css';
 
 // const socket =  io.connect('http://61.80.148.48')
-const socket =  io.connect('http://192.168.0.6')
+// const socket =  io.connect('http://192.168.0.6')
+const socket =  io.connect('http://192.168.50.102')
 
 
 function App() {
+    // 정보 입력 여부
+    const [isEnter, setIsEnter] = useState(false);
+    // 입력 이름
+    const [enterName, setEnterName] = useState(localStorage.getItem('uid'));
+    // 입력 번호
+    const [enterNumber, setEnterNumber] = useState(localStorage.getItem('number'));
+
+
+
+
+
+
     // 참가자 수
     const [users,setUsers] = useState(0);
     // 총 터치 수
@@ -24,14 +36,11 @@ function App() {
 
 
     useEffect(()=>{
-        console.log(1);
-
         //new Notification('ok', {body: 'good job!'});
-
         sendMessage('test');
 
         socket.on('message',data=>{
-            console.log(2 , data);
+            // console.log(2 , data);
 
             //sendMessage(Math.random());
             /*
@@ -42,7 +51,7 @@ function App() {
         });
         // 총터치수
         socket.on('touchMax',data=>{
-            console.log('touch : ' , data);
+            // console.log('touch : ' , data);
             setCount(data);
         });
         // 참가자 수
@@ -51,16 +60,16 @@ function App() {
         });
         // 입장
         socket.on('enter',data=>{
-            console.log( 'enter' ,  data )
+            // console.log( 'enter' ,  data )
         });
         // 참가자 정보
         socket.on('info',data=>{
-            console.log( 'info' ,  data )
+            // console.log( 'info' ,  data )
             setInfo(data);
         });
         // 탈락
         socket.on('die',data=>{
-            console.log( 'die' ,  data )
+            // console.log( 'die' ,  data )
 /*
             for(let uInfo of info) {
                 if (uInfo.info.name === data.name) {
@@ -70,16 +79,16 @@ function App() {
                 }
             }
 */
-            console.log( ' die info ' , info)
+            //console.log( ' die info ' , info)
             // setInfo([...info]);
         });
         // 퇴장
         socket.on('leave',data=>{
-            console.log( 'leave' ,  data )
+            //console.log( 'leave' ,  data )
 
             for(let uInfo of info) {
                 if (uInfo.info.name === data.name) {
-                    console.log('equal')
+                    // console.log('equal')
 
                     uInfo.info = data;
                 }
@@ -89,17 +98,12 @@ function App() {
         });
         // 멈춤
         socket.on('freeze',data=>{
-            console.log( 'freeze' ,  data )
+            //console.log( 'freeze' ,  data )
             setFreeze( data );
             setTouchMsg( !data );
         });
 
-    },[]);
-
-    useEffect(()=>{
-        console.log( info )
     },[info]);
-
 
     // 메시지 이벤트
     const sendMessage = data => {
@@ -114,14 +118,59 @@ function App() {
         setDie(true);
         socket.emit('die', true);
     };
+    // 커버 클릭 이벤트
+    const handleWelcomeClick = () => {
+        console.log(localStorage.getItem('uid'))
+
+        console.log(enterName);
+        console.log(enterNumber);
+
+        if (localStorage.getItem('uid') === null && enterName) {
+            localStorage.setItem('uid', enterName);
+        }
+        if (localStorage.getItem('uid') === null && enterNumber) {
+            localStorage.setItem('number', enterNumber);
+        }
+
+
+
+        setIsEnter(true);
+    };
 
     return (
         <>
             <div className="top">
-                <div className="entry">참가자 {users}명</div>
-                <h1 className={`txt${touchMsg}`}>무궁화 꽃이 피었습니다</h1>
+                <div className="entry"><div style={{color: 'white'}}>참가자</div> {users}명</div>
+                <h1 className={`txt${touchMsg}`} style={{color: 'white'}}><div style={{'display': 'none'}}>무궁화 꽃이 피었습니다</div></h1>
             </div>
             <div className="finish"></div>
+            <div className="enter-card">
+                <div className="cover">
+                    <div className={`welcome-img-${isEnter}`}>
+                        <input type="url"
+                               className="input-name"
+                               placeholder="your name"
+                               value={enterName}
+                               onChange={data => {
+                                   const value = data.target.value;
+                                   localStorage.setItem('uid', value);
+                                   setEnterName(value);
+                               }}
+                        ></input>
+                        <input type="number" pattern="\d*"
+                               className="input-number"
+                               placeholder="phone numer"
+                               value={enterNumber}
+                               onChange={data => {
+                                   const value = data.target.value;
+                                   localStorage.setItem('number', value);
+                                   setEnterNumber(value)
+                               }}
+                        ></input>
+                    </div>
+                    <div className={`welcome-img-${!isEnter} welcome-img`} onClick={handleWelcomeClick}>ㅇㅅㅁ</div>
+                </div>
+            </div>
             <div className={'textArea'} style={{width: '500px', height: '500px', background: 'black', display: 'none'}}>
                 <h1>anrndghk Rhcdl vldjTtmqslek</h1>
                 {/*<span>an</span>*/}
@@ -150,11 +199,26 @@ function App() {
                 {
                     info.length > 0 && info.map((data, i) => {
                         if(data.info.connect) {
+                            // 개선 대상, 접속중인 소켓으로 정렬
+                            const pos = Math.ceil(window.innerHeight / (users + 15));
+                            const idx = (() => {
+                                let tmpIdx = 0;
+
+                                for (let j = 0; j < i; j++) {
+                                    if (info[j].info.connect) tmpIdx++;
+                                }
+
+                                return tmpIdx;
+                            })();
+
+
                             if(data.info.die) {
-                                return (<div key={i} className="unit unit-die" style={{'left': `${data.info.count}%`, top: (i+1)*50 + 50}}>{data.info.index}</div>)
+                                return (<div key={i} className="unit unit-die" style={{'left': `${data.info.count}%`, top: (idx+1)*pos + 80}}>{data.info.index}</div>)
                             } else {
-                                return (<div key={i} className="unit" style={{'left': `${data.info.count}%`, top: (i+1)*50 + 50}}>{data.info.index}</div>)
+                                return (<div key={i} className="unit" style={{'left': `${data.info.count}%`, top: (idx+1)*pos + 80}}><div>{data.info.index}</div></div>)
                             }
+                        } else {
+                            return (<div key={i}></div>);
                         }
                     })
                 }
